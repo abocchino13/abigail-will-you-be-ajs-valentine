56390e3df4a5b8fdecc2162425c40fd3ff7786ae
abocchino13 <anthony.j.bocchino@gmail.com>
2026-02-09 11:03:55 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,15 @@ export default function Page() {
   const [noCount, setNoCount] = useState(0);
   const [yesClicks, setYesClicks] = useState(0);
   const [yesPressed, setYesPressed] = useState(false);
-  const yesButtonSize = noCount * 20 + 16;
-  const yesConfirmations = [
-  "Yes",
-  "Are you sure?",
-  "Sorry I was vacuuming and missed what you said",
-  "One more time? I am hard of hearing sorry",
-];
 
-  const handleNoClick = () => {
-    setNoCount(noCount + 1);
-  };
+  const yesConfirmations = [
+    "Yes",
+    "Are you sure?",
+    "Sorry I was vacuuming and missed what you said",
+    "One more time? I am hard of hearing sorry",
+  ];
+
+  const handleNoClick = () => setNoCount((c) => c + 1);
 
   const getNoButtonText = () => {
     const phrases = [
@@ -36,43 +34,50 @@ export default function Page() {
       "RIP",
       "No :(",
     ];
-
     return phrases[Math.min(noCount, phrases.length - 1)];
   };
+
+  const yesButtonSize = noCount * 20 + 16;
 
   return (
     <div className="-mt-16 flex h-screen flex-col items-center justify-center">
       {yesPressed ? (
         <>
-          <img src="https://media.tenor.com/gUiu1zyxfzYAAAAi/bear-kiss-bear-kisses.gif" />
-          <div className="my-4 text-4xl font-bold">WOOOOOO!!! I love you pookie!! ;))</div>
+          <img
+            alt="celebration"
+            src="https://media.tenor.com/gUiu1zyxfzYAAAAi/bear-kiss-bear-kisses.gif"
+          />
+          <div className="my-4 text-4xl font-bold">
+            WOOOOOO!!! I love you pookie!! ;))
+          </div>
         </>
       ) : (
         <>
           <img
+            alt="bear with roses"
             className="h-[200px]"
             src="https://gifdb.com/images/high/cute-love-bear-roses-ou7zho5oosxnpo6k.gif"
           />
           <h1 className="my-4 text-4xl">Will you be my Valentine?</h1>
-          <div className="flex items-center">
-              <button
-  onClick={() => {
-    if (yesClicks < yesConfirmations.length - 1) {
-      setYesClicks(prev => prev + 1);
-    } else {
-      setAccepted(true);
-    }
-  }}
->
-  {yesConfirmations[yesClicks]}
-</button>
 
+          <div className="flex items-center gap-4">
+            <button
+              onClick={() => {
+                if (yesClicks < yesConfirmations.length - 1) {
+                  setYesClicks((c) => c + 1);
+                } else {
+                  setYesPressed(true);
+                }
+              }}
+              className="rounded bg-green-500 px-4 py-2 font-bold text-white hover:bg-green-700"
+              style={{ fontSize: `${yesButtonSize}px` }}
             >
-              Yes
+              {yesConfirmations[yesClicks]}
             </button>
+
             <button
               onClick={handleNoClick}
-              className=" rounded bg-red-500 px-4 py-2 font-bold text-white hover:bg-red-700"
+              className="rounded bg-red-500 px-4 py-2 font-bold text-white hover:bg-red-700"
             >
               {noCount === 0 ? "No" : getNoButtonText()}
             </button>
